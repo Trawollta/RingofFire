89bdc9fc4b69d3652ee9e0170ed0aafc233d32c2
Semjasa83 <marceldezanna@yahoo.de>
2024-01-03 01:11:17 +0100

    fixed import

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
 
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule, 
+    PlayerComponent
+  ],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent implements OnInit{
 
   pickCardAnimation= false;
-  currentCard!: string;
+  currentCard: any;
   game!: Game;
-  name: string = '';
 
   constructor() {}
 
@@ -23,13 +26,13 @@ export class GameComponent implements OnInit{
     
   }
 
-  newGame(){
+  public newGame():void {
     this.game=new Game();
 
   }
 
 
-  takeCard() {
+  public takeCard(): void {
     if(!this.pickCardAnimation){
       this.currentCard = this.game.stack.pop();
       this.pickCardAnimation = true;
